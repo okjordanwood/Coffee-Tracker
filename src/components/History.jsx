044900f63800de62bd54c9361d680c8f786325a9
okjordanwood <jordancwood2002@gmail.com>
2025-06-15 19:48:25 -0400

    Updated History component

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,3 +1,44 @@
+import {
+  calculateCurrentCaffeineLevel,
+  coffeeConsumptionHistory,
+  getCaffeineAmount,
+  timeSinceConsumption,
+} from "../utils";
+
+// import { useAuth } from "../context/AuthContext";
+
 export default function History() {
-  return <></>;
+  // const { globalData } = useAuth();
+
+  return (
+    <>
+      <div className="section-header">
+        <i className="fa-solid fa-timeline" />
+        <h2>History</h2>
+      </div>
+      <p>
+        <i>Hover for more information!</i>
+      </p>
+      <div className="coffee-history">
+        {Object.keys(coffeeConsumptionHistory)
+          .sort((a, b) => b - a)
+          .map((utcTime, coffeeIndex) => {
+            const coffee = coffeeConsumptionHistory[utcTime];
+            const timeSinceConsumed = timeSinceConsumption(utcTime);
+            const originalAmount = getCaffeineAmount(coffee.name);
+            const remainingAmount = calculateCurrentCaffeineLevel({
+              [utcTime]: coffee,
+            });
+
+            const summary = `${coffee.name} | ${timeSinceConsumed} | ${coffee.cost} | ${remainingAmount}mg / ${originalAmount}mg`;
+
+            return (
+              <div title={summary} key={coffeeIndex}>
+                <i className="fa-solid fa-mug-hot" />
+              </div>
+            );
+          })}
+      </div>
+    </>
+  );
 }
